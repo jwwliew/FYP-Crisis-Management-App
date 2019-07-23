a80a87d123719d8b2551ed17132a869f2e852389
Bowen <1659550828@qq.com>
2019-07-23 15:44:39 +0800

    New plan name
    Data validation done

--- src/app/plans/new-plan/new-plan.module.ts
+++ src/app/plans/new-plan/new-plan.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
--- src/app/plans/new-plan/new-plan.page.ts
+++ src/app/plans/new-plan/new-plan.page.ts
@@ -1,8 +1,8 @@
 import { TemplateService } from 'src/app/services/template.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { PlanService } from './../../services/plan.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-plan',
@@ -10,27 +10,41 @@ import { PlanService } from './../../services/plan.service';
   styleUrls: ['./new-plan.page.scss'],
 })
 export class NewPlanPage implements OnInit {
+  @ViewChild('signupSlider') signupSlider;
+  public slideOneForm: FormGroup;
 
-  constructor(private router: Router, private PlanService: PlanService, private templateService: TemplateService) { }
+  public submitAttempt: boolean = false;
+
+  constructor(private router: Router, private PlanService: PlanService, private templateService: TemplateService, public formBuilder: FormBuilder) {
+
+    this.slideOneForm = formBuilder.group({
+      firstName: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+    })
+    console.log(this.submitAttempt);
+
+  }
 
   planName: any;
 
-
   ngOnInit() {
   }
-
   globalLanguage = this.templateService.globalLanguage;
   defaultLanguage = 0;
 
   nextPage() {
-
+    if (this.planName == undefined) {
+      return false;
+    }
+    else {
       console.log(this.planName, this.defaultLanguage)
       // this.router.navigate (
       //  ['/tabs/plans/details',this.planName]
       this.router.navigateByUrl('tabs/plans/details/' + this.defaultLanguage + '/' + this.planName);
       //)
+
     }
-  
+  }
+
   cancel() {
     this.router.navigateByUrl('');
   }
