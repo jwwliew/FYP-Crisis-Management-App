1b162e49744678b5ac6ff244136328f6cae08313
hiyongrui <172808H@mymail.nyp.edu.sg>
2019-08-06 12:14:20 +0800

    update tutorial pictures plans/templates align center image/text

--- src/app/settings/tutorial/tutorial.page.ts
+++ src/app/settings/tutorial/tutorial.page.ts
@@ -18,7 +18,8 @@ export class TutorialPage implements OnInit {
   selectedTab = "templates"
 
   templateList = ["assets/tutorial_template1.png", "assets/tutorial_template2.png", "assets/tutorial_template3.png", "assets/tutorial_template4.png", "assets/tutorial_template5.png", 
-                  "assets/tutorial_template6.png", "assets/tutorial_template7.png", "assets/tutorial_template8.png", "assets/tutorial_template9.png", "assets/tutorial_template10.png"]
+                  "assets/tutorial_template6.png", "assets/tutorial_template7.png", "assets/tutorial_template8.png", "assets/tutorial_template9.png", "assets/tutorial_template10.png",
+                  "assets/tutorial_template11.png", "assets/tutorial_template12.png"]
   planList = ["assets/tutorial_plan1.png", "assets/tutorial_plan2.PNG", "assets/tutorial_plan3.png", "assets/tutorial_plan4.png", "assets/tutorial_plan5.png", 
               "assets/tutorial_plan6.PNG", "assets/tutorial_plan7.png", "assets/tutorial_plan8.png", "assets/tutorial_plan9.png", "assets/tutorial_plan10.png", 
               "assets/tutorial_plan11.png", "assets/tutorial_plan12.png", "assets/tutorial_plan13.png"]
